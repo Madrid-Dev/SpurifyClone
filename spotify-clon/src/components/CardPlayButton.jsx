c3823d58e9e416f6bc8081c8d460e921f0237d11
Agustin Madrid <amadrid@quanam.com>
2024-02-15 20:21:26 -0300

    Finished tutorial

--- spotify-clon/src/components/CardPlayButton.jsx
+++ spotify-clon/src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Pause, Play } from './Player'
 import { usePlayerStore } from '../store/playerStore'
 
-export function CardPlayButton({id}){
+export function CardPlayButton({id, size = 'small'}){
 const {
     currentMusic,
     isPlaying,
@@ -37,10 +37,13 @@ const handleClick = () => {
     setIsPlaying(!isPlaying)
 }
 
-    return(
-        <div onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4" >
-            {isPlayingPlaylist ? <Pause/> : <Play/>} 
-        </div>
+    const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
+
+
+    return (
+    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
+        {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
+    </button>
     )
 
 }
